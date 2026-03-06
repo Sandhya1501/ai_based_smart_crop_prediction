5525d734edb124f78f710a88a58336759f3bedf5
Sandhya1501 <chinthakayalasandhya0@gmail.com>
2026-03-06 14:00:07 +0530

    added new student.py
    Marks of student

---
+++ student.py
@@ -0,0 +1,28 @@
+stu_dict ={}
+for i in range(3):
+    stuName = input ("Student Name : ")
+    stuMarks = int(input("student Marks : "))
+    stu_dict[stuName] = stuMarks
+print(stu_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
